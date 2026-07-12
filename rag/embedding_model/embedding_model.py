702dad013d1a9116339e706d78bd1536f43d3221
Nikolay Pautov <157311088+Pautovv@users.noreply.github.com>
2026-07-12 16:11:42 +0300

    feat: add custom exception to load_model func

--- rag/embedding_model/embedding_model.py
+++ rag/embedding_model/embedding_model.py
@@ -1,10 +1,26 @@
+import logging
 from sentence_transformers import SentenceTransformer
 
+from huggingface_hub.errors import RepositoryNotFoundError
+from requests.exceptions import ConnectionError
+from rag.exceptions import ModelLoadingError
+
+logger = logging.getLogger(__name__)
+
 def load_model(model_name):
-    model = SentenceTransformer(model_name)
-
-    return model
-
+    try:
+        return  SentenceTransformer(model_name)
+    except RepositoryNotFoundError as e:
+        logger.error(f'Model {model_name} not found on HuggingFace Hub')
+        raise ModelLoadingError(
+            f'Модель {model_name} не была найдена на HF Hub: проверьте название.'
+        ) from e 
+    except ConnectionError as e:
+        logger.error(f'Network error while downloading {model_name}')
+        raise ModelLoadingError(
+            f'Не удалсоь скачать модель: проблемы с сетью.'
+        ) from e
+    
 def encode(sentences, model):
     embeddings = model.encode(sentences)
     
--- rag/exceptions.py
+++ rag/exceptions.py
@@ -1,6 +1,7 @@
 class RAGError(Exception): pass
 
 class DatasetParsingError(RAGError): pass
+class ModelLoadingError(RAGError): pass
 class IndexNotFoundError(RAGError): pass
 class MetadataNotFoundError(RAGError): pass
 class EmptyRetrieverError(RAGError): pass
